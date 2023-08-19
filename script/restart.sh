a369765ac14f93ed5c41f449c1d18a8d10b40d0e
Hiroya Onoe <git@onoe.dev>
2023-08-19 15:10:54 +0900

    typo

--- script/restart.sh
+++ script/restart.sh
@@ -6,4 +6,4 @@ cd ~/webapp/go
 go build
 sudo systemctl restart isucondition.go.service
 sudo systemctl restart nginx.service # reloadでも良い
-sudo systemctl reater mysql.service
+sudo systemctl restart mysql.service
